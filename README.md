3bb1feebe3c49bdd53b37b60cbefd78cb70b721d
Ash Amin <ashtheamin@gmail.com>
2023-02-15 08:41:03 +1100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Libry
-A library web application
+
+A library web application built around a Flask server.
+
+
+
+## Dependencies
+[Third Party Licenses and Credits](docs/3P.md)
+
+Depends on Flask and Python being installed.
--- src/server.py
+++ src/server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path=('/static'))
 
 @app.route("/")
 def index():
-    return app.static_url_path('index.html')
+    return app.send_static_file('index.html')
